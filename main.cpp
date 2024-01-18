7cc2a0a70bdf4578e47fa2bbb8bd576b3932293e
Ninek <mbakala@kis.p.lodz.pl>
2024-01-18 17:55:46 +0100

    Nowa metoda liczenia mnożenie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+int Policz(int a, int b){
+    return a*b;
+
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "trzeci commit" << std::endl;
+    std::cout << "W pierwszym branchu a*b" << Policz(10,4) << std::endl;
     return 0;
 }
