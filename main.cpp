6aeb8457aa6491b34fe3fbdae9226a474491d731
Ninek <mbakala@kis.p.lodz.pl>
2024-01-18 17:34:36 +0100

    Nowa metoda liczenia

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+int Policz(int a, int b){
+  return a+b;
+
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W pierwszym branchu a+b" << Policz(3,4) << std::endl;
     return 0;
 }
