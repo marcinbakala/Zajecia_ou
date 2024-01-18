8427cde979498c9bf9816f47963a86e63b15c3ae
Ninek <mbakala@kis.p.lodz.pl>
2024-01-18 17:46:31 +0100

    Nowa metoda liczenia plus dodatkowy komentarz

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int Policz(int a, int b){
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    d::cout << "drugi commit" << std::endl;
     std::cout << "W pierwszym branchu a+b" << Policz(3,4) << std::endl;
     return 0;
 }
